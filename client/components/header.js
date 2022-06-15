69f43ff9349385534c4e5144fa7995adbcbb4fbf
mfadlanridho <devmfadlanridho@gmail.com>
2022-06-16 00:28:12 +0700

    client fixes

--- client/components/header.js
+++ client/components/header.js
@@ -22,7 +22,7 @@ export default ({ currentUser }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
-        <a className="navbar-brand">Ticket App</a>
+        <a className="navbar-brand">Tiketku</a>
       </Link>
 
       <div className="d-flex justify-content-end">
--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -37,7 +37,7 @@ const OrderShow = ({ order, currentUser }) => {
       Time left to pay: {timeLeft} seconds
       <StripeCheckout
         token={({ id }) => doRequest({ token: id })}
-        stripeKey="pk_test_JMdyKVvf8EGTB0Fl28GsN7YY"
+        stripeKey="pk_test_51LAgkqEg7gwOzpWPMuQpVOWW3lJO8P9jx7gn1HK1y0FKIGYd71GfDPFCTBlB7pVNnf5AdgHrLuBpuhSO5caNFIMT0061pvAFDp"
         amount={order.ticket.price * 100}
         email={currentUser.email}
       />
